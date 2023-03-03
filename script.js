482ffd5c169a77371ac8a578c5e671e276d128ff
Noah Patullo <macrobyte2@gmail.com>
2023-03-03 06:19:11 -0500

    Add copy and paste

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const input = document.querySelector("#textInput");
 const output = document.querySelector("#output");
 
-//TODO handle code injection/execution
+//TODO handle code injection/execution?
 
 input.addEventListener("input", (event) => {
-    output.value = input.value?.toLowerCase();
+	output.value = input.value?.toLowerCase();
 });
 
 output.addEventListener("focus", function() {
-    this.select();
+	this.select();
+});
+
+const pasteButton = document.querySelector("#pasteButton");
+const copyButton = document.querySelector("#copyButton");
+
+//implemented as overwriting everything, handling prior cursor position seems out of scope
+pasteButton.addEventListener("click", () => {
+	console.log('paste click')
+	
+	navigator.clipboard
+	.readText()
+	.then((clipText) => {
+		input.value = clipText;
+		output.value = input.value?.toLowerCase();
+	})
+	.catch(err => console.error('Failed to paste.', err));
+});
+
+copyButton.addEventListener("click", () => {
+	navigator.clipboard
+	.writeText(output.value)
+	.then(() => {
+	//   console.log("Wrote stuff to clipboard");
+	},
+	(err) => {
+	  console.error('Failed to write to clipboard.', err);
+	})
+	//unclear if this way of handling promise failure is better
+	.catch(err => console.error('Failed to write to clipboard.', err));
 });
